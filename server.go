5744344369004cdb1e83bbf7e8cd16e7e821ef25
rinaldaj <rinaldaj@clarkson.edu>
2018-04-21 17:04:39 -0400

    now stores and delets json properly in live mode

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"bufio"
 	"strings"
+	"os"
+	"sync"
 )
 
 type task struct {
@@ -16,6 +18,12 @@ type task struct {
 	Duetime	string
 	Completiontime	int
 }
+	var mutx = &sync.Mutex{} //makes sure that threads aren't fighting over jason
+	var jason = make([]task,0) //stores the objects that are parsed in
+
+func eq(x task, y task) bool{
+	return strings.EqualFold(x.User, y.User) && x.Importance == y.Importance && strings.EqualFold(x.Task, y.Task) && strings.EqualFold(x.Duedate,y.Duedate) && strings.EqualFold(x.Duetime,y.Duetime) && x.Completiontime == y.Completiontime
+}
 
 func doTheThing(connection net.Conn){
 	recept, err := bufio.NewReader(connection).ReadString('\n')
@@ -24,24 +32,56 @@ func doTheThing(connection net.Conn){
 		return
 	}
 	strs := strings.Split(recept,"{") //Splits the string along the { to divide up the json
-	jason := make([]task,0) //stores the objects that are parsed in
 	if (recept[0] == 49 || recept[0] == 50){ //Ascii value for 1 (change after testing)
 		for i := 1; i < len(strs);i++{
-			//This loop goes through the split string and creates json objects
+			//This loop goes through the split string and creates json compatible strings
 			strs[i] = "{" + strs[i]
-			tmp := task{}
-			json.Unmarshal([]byte(strs[i]),&tmp)
-			jason = append(jason,tmp)
 		}
 	} else {
 		//Send the list of tasks
+		fmt.Println("test")
+	}
+
+	if (recept[0] == 49){
+		for i:=1;i<len(strs);i++{
+			//parses strings and adds them to json
+			tmp := task{}
+			json.Unmarshal([]byte(strs[i]),&tmp)
+			mutx.Lock()
+			jason = append(jason,tmp)
+			mutx.Unlock()
+		}
+	} else {
+		mutx.Lock()
+		tmper := make([]task,0)
+		for i:=0;i<len(jason);i++ {
+			for j:=1;j<len(strs);j++{
+				tmp := task{}
+				json.Unmarshal([]byte(strs[j]),&tmp)
+				if(!eq(tmp,jason[i])){
+					tmper = append(tmper,jason[i])
+				}
+			}
+		}
+		jason = tmper
+		mutx.Unlock()
 	}
 	fmt.Println(jason)
 	connection.Close()
+	return
 }
 
 func main(){
-	ln, _ := net.Listen("tcp",":6666")
+	args := os.Args
+	if(len(args) != 2){
+		fmt.Println("Useage:",args[0]," <port>")
+		return
+	}
+	ln, errs := net.Listen("tcp", ":" + args[1])
+	if (errs != nil){
+		fmt.Println("something went wrong when binding the socket")
+		return
+	}
 	for {
 		conn, err := ln.Accept()
 		if(err != nil){
